15aaef4e09a040e73c9482f957217457ee7e944e
David Ericson <davidestudofcg@gmail.com>
2026-02-02 20:31:02 -0300

    feat: add loginController to auth controller
    Import loginUser and add loginController in src/controllers/auth.controller.js to handle user login requests.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import { createUser } from "../services/auth.service.js";
+import { createUser, loginUser } from "../services/auth.service.js";
 
 export async function createUserController(req, res) {
     try {
@@ -11,8 +11,32 @@ export async function createUserController(req, res) {
         if (err.code == "23505") {
             return res.status(409).json({message: "Usuário já existe"});
         }
-        
+
         console.error(err)
         return res.status(500).json({message: "Erro ao criar usuário"})
+    }
+}
+
+export async function loginController(req, res) {
+    try {
+        const {username, password} = req.body;
+
+        const user = await loginUser({username, password})
+
+        return res.status(200).json(user);
+    } catch (err) {
+        if (
+            err.message === "Dados inválidos" ||
+            err.message === "Usuário ou senha inválidos"
+        ) {
+            return res.status(401).json({
+                message: err.message,
+            });
+        }
+
+        console.error(err);
+        return res.status(500).json({
+            message: "Erro ao fazer login",
+        });
     }
 }
