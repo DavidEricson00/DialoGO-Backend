aa39dc689442379b2ae2f3e683bd481ca82a2881
David Ericson <davidestudofcg@gmail.com>
2026-02-02 20:30:22 -0300

    feat: add loginUser
    Import findUserByUsername from the user repository, replace parseInt with Number for BYCRYPT_SALT_ROUNDS, and add a new loginUser service. The loginUser function validates inputs, looks up the user, compares the provided password with the stored hash using bcrypt, removes the password field from the returned user, and throws clear errors on invalid data or credentials.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,16 @@
 import bcrypt from "bcrypt"
 import { BYCRYPT_SALT_ROUNDS } from "../config/env.js";
-import { createUser as createUserRepo } from "../repository/user.repository.js"
+import { 
+    createUser as createUserRepo,
+    findUserByUsername as findUserByUsernameRepo,
+}  from "../repository/user.repository.js"
 
 export async function createUser({username, password}) {
     if(!username || !password) {
         throw new Error("Dados inválidos");
     }
 
-    const saltRounds = parseInt(BYCRYPT_SALT_ROUNDS);
+    const saltRounds = Number(BYCRYPT_SALT_ROUNDS);
 
     const passwordHash = await bcrypt.hash(password, saltRounds);
 
@@ -22,4 +25,31 @@ export async function createUser({username, password}) {
         avatar: user.avatar,
         created_at: user.created_at
     }
+}
+
+export async function loginUser({username, password}) {
+    if(!username || !password) {
+        throw new Error("Dados inválidos");
+    }
+
+    const saltRounds = Number(BYCRYPT_SALT_ROUNDS);
+
+    const user = await findUserByUsernameRepo(username);
+
+    if(!user) {
+        throw new Error("Usuário ou senha inválidos");
+    }
+
+    const passwordMatch = await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if(!passwordMatch) {
+        throw new Error("Usuário ou senha inválidos");
+    }
+
+    delete user.password;
+
+    return user;
 }
