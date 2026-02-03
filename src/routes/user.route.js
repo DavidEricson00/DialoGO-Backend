4ad89c0edc06b28b527f8aaa2cdd2334358df8a3
David Ericson <davidestudofcg@gmail.com>
2026-02-02 21:50:39 -0300

    feat: add authenticated /me user route
    Update user.route.js to import getMeController from user.controller.js and authMiddleware, and register a new GET /me route protected by authMiddleware.

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createUserController, loginController } from "../controllers/auth.controller.js";
+import { createUserController, getMeController, loginController } from "../controllers/user.controller.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 
 const router = Router();
 
 router.post("/create", createUserController);
 router.post("/login", loginController);
+router.get("/me", authMiddleware, getMeController)
 
 export default router;
