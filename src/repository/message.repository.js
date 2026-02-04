91fb948316ef70b8a7d86143903907355e212216
David Ericson <davidestudofcg@gmail.com>
2026-02-04 16:44:27 -0300

    feat: rename messages repo file; return full rows

--- src/repository/message.repository.js
+++ src/repository/message.repository.js
@@ -10,14 +10,14 @@ export async function getChatMessages(chatId) {
     const values = [chatId];
 
     const {rows} = await pool.query(query, values);
-    return rows[0];
+    return rows;
 }
 
 export async function sendMessage(content, chatId, userId) {
     const query = `
         INSERT INTO messages (content, chat_id, user_id)
         VALUES($1, $2, $3)
-        RETURNING content, sent_at
+        RETURNING content, sent_at, chat_id, user_id
     `;
 
     const values = [content, chatId, userId];
