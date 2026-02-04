114ce6c0778a9983baf90cd1f168c7337c17d86c
David Ericson <davidestudofcg@gmail.com>
2026-02-04 16:46:22 -0300

    feat: add message routes and mount to router

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./user.route.js";
+import messageRouter from "./message.route.js"
 
 const router = Router();
 
 router.use("/user", userRouter);
+router.use("/message", messageRouter)
 
 export default router;
---
+++ src/routes/message.route.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { 
+    getChatMessagesController, 
+    sendMessageController 
+} from "../controllers/message.controller";
+import { authMiddleware } from "../middleware/authMiddleware";
+
+const router = Router();
+
+router.get("/chat/:chatId", authMiddleware, getChatMessagesController);
+router.post("/", authMiddleware, sendMessageController);
+
+export default router
