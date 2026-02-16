ba2de5390db3c94b36da4fb192df4b1eb1e0394f
David Ericson <davidestudofcg@gmail.com>
2026-02-16 14:39:10 -0300

    feat: remove sendMessage controller and route

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -3,32 +3,10 @@ import { getChatMessages, sendMessage } from "../services/messages.service.js";
 export async function getChatMessagesController(req, res) {
     try {
         const { chatId } = req.params;
-        
         const chatMessages = await getChatMessages(chatId);
-
         return res.json(chatMessages);
     } catch(err) {
         console.error(err);
         return res.status(404).json({message: "Erro ao buscar mensagens"});
-    }
-}
-
-export async function sendMessageController(req, res) {
-    try {
-        const { content, chatId } = req.body;
-
-        const message = await sendMessage({ 
-            content, 
-            chatId, 
-            userId: req.user.id 
-        });
-
-        const roomName = `chat:${chatId}`;
-        io.to(roomName).emit("newMessage", message)
-
-        return res.status(201).json(message);
-    } catch (err){
-        const status = err.statusCode || 500;
-        return res.status(status).json({ message: err.message });
     }
 }
--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,14 +1,11 @@
 import { Router } from "express";
 import { 
-    getChatMessagesController, 
-    sendMessageController 
+    getChatMessagesController 
 } from "../controllers/message.controller.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.get("/chat/:chatId", authMiddleware, getChatMessagesController);
-
-router.post("/", authMiddleware, sendMessageController);
 
 export default router
